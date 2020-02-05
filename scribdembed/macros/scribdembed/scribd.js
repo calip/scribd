43c2495bd5105d9d6078e7aa9173f1d95833a6fa
alip.putra <alip.putra@schlix.com>
2020-02-05 20:11:28 +0700

    handle empty innerhtml

--- scribdembed/macros/scribdembed/scribd.js
+++ scribdembed/macros/scribdembed/scribd.js
@@ -13,23 +13,26 @@ function scribd_render()
 
     function processScribdext()
     {
-        var text = $('[itemprop="articleSection"]')[0].innerHTML;
-        var matches = findAllScribd(/<[a][^>]*>(https?:\/\/(?:www\.)?scribd\.com\/(?:.*doc|document)\/([^\/]+).*)<\/[a]>/, text);
+        var element = $('[itemprop="articleSection"]')[0];
+        if(element){
+            var text = element.innerHTML;
+            var matches = findAllScribd(/<[a][^>]*>(https?:\/\/(?:www\.)?scribd\.com\/(?:.*doc|document)\/([^\/]+).*)<\/[a]>/, text);
 
-        if (matches.length > 0) {
-            for (var i = 0; i < matches.length; i++) {
-                var scribd_tag = matches[i][0];
-                var scribd_url = matches[i][1];
-                var scribd_id = matches[i][2];
+            if (matches.length > 0) {
+                for (var i = 0; i < matches.length; i++) {
+                    var scribd_tag = matches[i][0];
+                    var scribd_url = matches[i][1];
+                    var scribd_id = matches[i][2];
 
-                if (scribd_url != null && scribd_id != null){
-                    var iframe = '<iframe width="100%" height="600" src="https://www.scribd.com/embeds/'+ scribd_id +'/content?start_page=1&view_mode=lis" frameborder="0" allowfullscreen></iframe>';
+                    if (scribd_url != null && scribd_id != null){
+                        var iframe = '<iframe width="100%" height="600" src="https://www.scribd.com/embeds/'+ scribd_id +'/content?start_page=1&view_mode=lis" frameborder="0" allowfullscreen></iframe>';
 
-                    text = text.replace(scribd_tag, iframe);
+                        text = text.replace(scribd_tag, iframe);
+                    }
                 }
             }
+            element.innerHTML = text;
         }
-        $('[itemprop="articleSection"]')[0].innerHTML = text;
     }  
     processScribdext();
 }
